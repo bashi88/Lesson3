15d095bd9d221c57d2d945a3de308ab9bd6bfc8f
unknown <sienk001@D0117108.wurnet.nl>
2015-01-07 16:49:07 +0100

    leapyearv3 changed x to year and warning back to stop as warning message did not function properly

--- R/Leapyear.R
+++ R/Leapyear.R
@@ -7,9 +7,9 @@ leapyear <- function(year) {
   #Function to identify if the specified year is a Leap Year
   # year specifies the year of interest
   if(!is.numeric(year)){
-    warning("x must be of class numeric")}
+    stop("year must be of class numeric")}
   else {if(year< 1582){
-    warning ("year is to early for leap year calendar")}}
+    stop ("year is to early for leap year calendar")}}
   #year is numeric and within range identify if specified year was a leap year
   if (year %% 4 != 0) {return(FALSE)}
   else {if(year %% 100 != 0) {return (TRUE)}
