3797ec61481f9f68f7e46a6b17814db114195d4e
unknown <sienk001@D0117108.wurnet.nl>
2015-01-07 16:32:01 +0100

    leapyear v2

--- R/Leapyear.R
+++ R/Leapyear.R
@@ -1,10 +1,15 @@
+#Team: ZeaPol   
+#Team Members: Roeland de Koning / Barbara Sienkiewicz    
+#Date: 07/01/2015       
+#Exercise 3
+
 leapyear <- function(year) {
   #Function to identify if the specified year is a Leap Year
   # year specifies the year of interest
   if(!is.numeric(year)){
-    stop("x must be of class numeric")}
+    warning("x must be of class numeric")}
   else {if(year< 1582){
-    stop ("year is to early for leap year calendar")}}
+    warning ("year is to early for leap year calendar")}}
   #year is numeric and within range identify if specified year was a leap year
   if (year %% 4 != 0) {return(FALSE)}
   else {if(year %% 100 != 0) {return (TRUE)}
